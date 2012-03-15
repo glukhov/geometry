dd47172d6c89c40d98ee8fdcaed450a57fcc74fa
glukhov <glukhov_1992@mail.ru>
2012-03-15 21:22:12 +0300

    still not work

--- merge_hull/main.cpp
+++ merge_hull/main.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 #define SIZE 5
-//static point_type pole;
+
 
 
 class point_type
@@ -26,19 +26,83 @@ public:
 
 };
 
+//OXY, return 1 if X < Y
+int triangle_area_2 (point_type p1, point_type p2, point_type p3) {
+	int s = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+	if(s > 0)
+		return 1;
+	else if(s < 0)
+		return -1;
+	else
+		return 0;
+}
+
 double get_length(point_type p1, point_type p2)
 {
-    return sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    return sqrt(double((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)));
 }
 
 double get_angle(point_type p1, point_type p2)
 {
-    return (p1.x - p2.x)/get_length(p1, p2);
+    
+	return (p1.x - p2.x)/get_length(p1, p2);
     //return (p2.x - p1.x) / (p2.y - p1.y);
 }
 
 
+int is_left(point_type p1, point_type p2, point_type p3)
+{
+    return (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y);
+}
 
+bool angle_compare(point_type  pole,point_type  i, point_type  j)
+{
+	if(is_left(pole, i , j) == 0)
+		{
+			if( (i.y > pole.y && j.y > pole.y) || (i.y < pole.y && j.y < pole.y) )
+			{
+				return (get_length(i, pole) < get_length(j, pole));
+			}
+			else if(i.y = pole.y && j.y == pole.y)
+			{
+				if( (i.x > pole.x && j.x > pole.x) || (i.x < pole.x && j.x < pole.y) )
+					return (get_length(i, pole) < get_length(j, pole));
+				else
+					return (i.x < j.x);
+			}
+			else
+			{
+				return (i.y > j.y);
+			}
+		}
+		else
+		{
+			if(is_left(pole, i, j) > 0)
+			{
+				if(i.y >= pole.y)
+					return 1;
+				else
+				{
+					if(j.y < pole.y)
+						return 1;
+					else
+						return 0;
+				}
+			}
+			else
+			{
+				if(i.y < pole.y)
+					return 0;
+				else
+				{
+					if(j.y >= pole.y)
+						return 0;
+					else
+						return 1;
+				}
+			}
+		}
+}
 
 class sort_class
 {
@@ -50,11 +114,20 @@ public:
     }
 
     bool operator() (point_type i, point_type j)
-    {
-        if(get_angle(i, pole) == get_angle(j, pole))
+	{
+      /*  if(get_angle(i, pole) == get_angle(j, pole))
             return (get_length(i, pole) < get_length(j, pole)) ;
         else
             return (get_angle(i, pole) >  get_angle(j, pole));
+			
+			*/
+	/*	if(triangle_area_2(pole, i, j) == 0)
+            return (get_length(i, pole) < get_length(j, pole)) ;
+        else
+            return (triangle_area_2(pole, i ,j)  > 0);
+			*/
+		
+		return angle_compare(pole, i, j);
     }
 };
 
@@ -62,10 +135,6 @@ public:
 
 
 
-int is_left(point_type p1, point_type p2, point_type p3)
-{
-    return (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y);
-}
 
 std::vector<point_type> graham_scan(std::vector<point_type> points, point_type pole)
 {
@@ -74,17 +143,31 @@ std::vector<point_type> graham_scan(std::vector<point_type> points, point_type p
     st.push_back(points[points.size() - 1]);
     st.push_back(pole);
 
-    for(int i = 1; i < points.size() - 1; i++)
+    for(int i = 0; i < points.size() - 1; i++)
     {
-        if(is_left(st[st.size() - 2], st[st.size() - 1], points[i] ) > 0)
+        int left = is_left(st[st.size() - 2], st[st.size() - 1], points[i] );
+		if( left > 0)
         {
             st.push_back(points[i]);
         }
-        else
+      /*else if(left == 0)
         {
+			st.pop_back();
+            st.push_back(points[i]);
+
+		}*/
+		else
+		{
             st.pop_back();
+			i--;
         }
     }
+	//test of 3 last points on one line
+	if(is_left(st[st.size() - 2], st[st.size() - 1], points[points.size() - 1]) == 0)
+	{
+		st.pop_back();
+	}
+
     return st;
 
 }
@@ -133,13 +216,47 @@ point_type centroid(point_type p1, point_type p2, point_type p3)
     return p;
 }
 
+int in_polygon(std::vector<point_type> polygon, point_type p)
+{
+    for(int i = 0; i < polygon.size() - 1; i++)
+    {
+        if(is_left(polygon[i], polygon[i + 1], p) < 0)
+            return 0;
+    }
+    return 1;
+}
+
+std::vector<point_type> merge_poligons(std::vector<point_type> set1, std::vector<point_type> set2, point_type pole)
+{
+		int min1 = 0;
+		int min2 = 0;
+		for(int i = 0; i < set1.size(); i++)
+		{
+			if(angle_compare(pole, set1[i], set1[min1]))
+				min1 = i;
+		}
+		for(int i = 0; i < set2.size(); i++)
+		{
+			if(angle_compare(pole, set2[i], set2[min2]))
+				min2 = i;
+		}
+
+}
+
 std::vector<point_type> merge(std::vector<point_type> set1, std::vector<point_type> set2)
 {
-  //  point_type p = centroid(set1[0], set1[1], set1[2]);
+    point_type pole;
+	point_type p = centroid(set1[0], set1[1], set1[2]);
     std::vector<point_type> sum;
-  //  if(in_polygon(set2, p) != 0)
-  //  {
-        point_type pole;
+	sum.assign(set1.begin(), set1.end());
+    for(int i = 0; i < set2.size(); i++)
+	{
+		sum.push_back(set2[i]);
+	}
+
+    if(in_polygon(set2, p) != 0)
+    {
+        /*
         int pole_pos;
         int in_set = 0;
         int pole1 = get_pole(set1);
@@ -175,12 +292,12 @@ std::vector<point_type> merge(std::vector<point_type> set1, std::vector<point_ty
         int min2 = 0;
         for(int i = 0; i < set1.size(); i++)
         {
-            if(angles1[i] > min1)
+            if(angles1[i] > angles1[min1])
                 min1 = i;
         }
         for(int i = 0; i < set2.size(); i++)
         {
-            if(angles2[i] > min2)
+            if(angles2[i] > angles2[min2])
                 min2 = i;
         }
 
@@ -223,28 +340,25 @@ std::vector<point_type> merge(std::vector<point_type> set1, std::vector<point_ty
                 }
             }
         }
-  //  }
- //   else
-  //  {
-           //not_in
-  //  }
-    return graham_scan(sum, pole);
+		*/
+
+   }
+    else
+    {
+           return get_hull(sum);
+    }
+  
+	return graham_scan(sum, pole);
+	
+	//return get_hull(sum);
 }
 
-int in_polygon(std::vector<point_type> polygon, point_type p)
-{
-    for(int i = 0; i < polygon.size() - 1; i++)
-    {
-        if(is_left(polygon[i], polygon[i + 1], p) < 0)
-            return 0;
-    }
-    return 1;
-}
+
 
 std::vector<point_type> merge_hull(std::vector<point_type> set)
 {
 
-    if (set.size() < SIZE)
+    if (set.size() <= SIZE)
     {
         return get_hull(set);
     }
@@ -253,7 +367,7 @@ std::vector<point_type> merge_hull(std::vector<point_type> set)
     std::vector<point_type> set1;
     set1.assign(set.begin(), set.begin() + set.size() / 2);
     std::vector<point_type> set2;
-    set2.assign(set.begin() + set.size() / 2 + 1, set.end());
+    set2.assign(set.begin() + set.size() / 2 , set.end());
 
     std::vector<point_type> hull1 = merge_hull(set1);
     std::vector<point_type> hull2 = merge_hull(set2);
@@ -272,7 +386,9 @@ int main()
     std::vector<point_type> a;
     std::vector<point_type> b;
     int n;
-    freopen("in.txt", "r", stdin);
+
+
+	freopen("in.txt", "r", stdin);
     freopen("out.txt", "w", stdout);
     std::cin >> n;
     for(int i = 0; i < n; i++)
@@ -282,7 +398,13 @@ int main()
         a.push_back(point_type(x, y));
     }
     b = get_hull(a);
-
+//	int t = is_left(a[0], a[1], a[2]);
+/*
+    point_type pole (0, 0);
+	sort_class sorter(pole);
+    std::sort(a.begin(), a.end(), sorter);
+*/	
+	//std::cout << t;
     for(int i = 0; i < b.size(); i++)
     {
         std::cout << b[i].x << "  " << b[i].y << std::endl;
