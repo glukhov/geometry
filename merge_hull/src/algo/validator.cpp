db56fad1bbe134f4c0e83a1da33ca271917cc9a9
Alexander Glukhov <glukhov_1992@mail.ru>
2012-04-08 19:45:59 -0400

    log(n) checking

--- merge_hull/src/algo/validator.cpp
+++ merge_hull/src/algo/validator.cpp
@@ -10,7 +10,7 @@ Validator::Validator()
 void Validator::generate()
 {
 
-    srand ( rand() );
+    srand ( time(NULL) );
     int a, b;
     for(int i = 0; i < this->test_size; i++)
     {
@@ -36,6 +36,7 @@ void Validator::get_hull()
 
 bool Validator::test()
 {
+    std::cout << "start test" << std::endl;
     if(points.size() == 1 && convex_hull.size() == 1)
         return true;
     if(points.size() == 2 && convex_hull.size() == 2)
@@ -60,6 +61,8 @@ bool Validator::test()
         }
     }
 
+
+/*
     for(int i = 0; i < points.size(); i++)
     {
         if(!is_hull[i])
@@ -71,6 +74,47 @@ bool Validator::test()
             }
         }
     }
+*/
+
+
+
+    Point2Df pole = centroid(convex_hull[0], convex_hull[1], convex_hull[2]);
+    for(int i = 0; i < points.size(); i++)
+    {
+        if(!is_hull[i])
+        {
+            Point2D p = points[i];
+
+            int min = 0;
+            int max = convex_hull.size() - 1;
+            int mid = (min + max) / 2;
+            while(max != 0 && mid != convex_hull.size() - 1)
+            {
+
+                mid = (min + max) / 2;
+                if(angle_compare(pole, convex_hull[mid], p) && angle_compare(pole, convex_hull[mid + 1], p))
+                    min = mid + 1;
+                else if(!angle_compare(pole, convex_hull[mid], p) && !angle_compare(pole, convex_hull[mid + 1], p))
+                    max = mid;
+                else
+                    break;
+            }
+            if(max == 0)
+                mid = convex_hull.size() - 1;
+
+            if(is_left(convex_hull[mid], convex_hull[get_index( mid + 1, convex_hull.size())], p) < 0)
+            {
+               // std::cout << pole.getX() << "   " << pole.getY() << "  pole" << std::endl;
+                std::cout << p.getX() << "   " << p.getY() << "  not in poly near edge" <<  std::endl;
+                std::cout << convex_hull[mid].getX() << "   " << convex_hull[mid].getY() << "            "
+                          << convex_hull[get_index( mid + 1, convex_hull.size())].getX()<< "   "
+                          << convex_hull[get_index( mid + 1, convex_hull.size())].getY() << std::endl;
+
+                return false;
+            }
+        }
+    }
+
 
     std::cout << "all points are in hull" << std::endl << "all is OK" << std::endl << std::endl;
     return true;
